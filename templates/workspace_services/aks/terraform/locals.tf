a54a9fc6fabd89ca7a17f6aff8c2e9d95436937c
Jade Wilson <jadewilson@microsoft.com>
2025-07-30 15:26:01 +0000

    Remove unused tre_workspace_service_tags

--- templates/workspace_services/aks/terraform/locals.tf
+++ templates/workspace_services/aks/terraform/locals.tf
@@ -3,10 +3,4 @@ locals {
   short_workspace_id           = substr(var.workspace_id, -4, -1)
   service_resource_name_suffix = "${var.tre_id}-ws-${local.short_workspace_id}-svc-${local.short_service_id}"
   core_resource_group_name     = "rg-${var.tre_id}"
-
-  tre_workspace_service_tags = {
-    tre_id                   = var.tre_id
-    tre_workspace_id         = var.workspace_id
-    tre_workspace_service_id = var.id
-  }
 }
