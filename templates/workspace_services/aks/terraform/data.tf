bd316145a62d609e75fc81f5acbb28f708942b3f
Jade Wilson <jadewilson@microsoft.com>
2025-09-08 09:12:31 +0000

    Merged PR 9480: Workloads not deploying bug
    There were two issues that were occuring:
    
    1. Selecting the first DNS zone with the name rather than the one in our resource group, this meant when we have multiple TRE's in the same sub it was just selecting the first one.
    2. HF_TOKEN causing an issue when not set as a variable, passed in an obvious default variable so that this doesn't happen. In future we would want to make this visible through the UI depending on whether we choose DSOP, AI or KG.
    
    ----
    #### AI description  (iteration 2)
    #### PR Classification
    Bug fix addressing workload deployment issues.
    
    #### PR Summary
    This pull request resolves deployment failures by updating configuration files to ensure proper service versioning and resource referencing.
    - `/templates/workspace_services/aks/porter.yaml`: The service version is bumped from 1.0.0 to 1.0.2.
    - `/templates/workspace_services/aks/terraform/data.tf`: Added the `resource_group_name` attribute to the `azurerm_private_dns_zone` data block.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Related work items: #60058

--- templates/workspace_services/aks/terraform/data.tf
+++ templates/workspace_services/aks/terraform/data.tf
@@ -38,6 +38,7 @@ data "azurerm_log_analytics_workspace" "tre" {
 
 data "azurerm_private_dns_zone" "aks" {
   name = "privatelink.${data.azurerm_resource_group.ws.location}.azmk8s.io"
+  resource_group_name = local.core_resource_group_name
 }
 
 data "azurerm_key_vault" "ws" {
